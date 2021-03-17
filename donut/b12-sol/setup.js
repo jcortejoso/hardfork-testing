040c1c3519cb658073c5958d936da27c007387d7
Or Neeman <or@clabs.co>
2021-03-17 14:49:32 -0600

    Make b12-sol accept config from env variables.
    Also: (a) fund 3 validator accounts in genesis

--- donut/b12-sol/setup.js
+++ donut/b12-sol/setup.js
@@ -5,30 +5,18 @@ const { sleep } = require("@celo/base")
 
 const devAccounts = JSON.parse(fs.readFileSync("../localenv/dev-accounts.json"))
 
-const url ="http://localhost:8545"
-const privateKey = devAccounts[0].privateKey
-const address = devAccounts[0].address
+const url = process.env.RPC_URL || "http://localhost:8545"
+const address = process.env.FUNDED_ADDRESS || "0xd927E9e733a46f46e18528148D8b98E496545f66" // validator 0
 const kit = Kit.newKit(url)
 let gethAddress
 
 module.exports = {url, privateKey, address}
 
 before(async () => {
-  const accounts = await kit.web3.eth.personal.getAccounts()
-  gethAddress = accounts[0]
-  const balance = await kit.connection.getBalance(gethAddress)
-  if (balance.length < 18) {
-    await kit.connection.addAccount(privateKey)
-    await kit.sendTransaction({
-      from: address,
-      to: gethAddress,
-      value: Web3.utils.toWei("1", "ether").toString()
-    })
-    await sleep(5000)
-  }
-  await kit.web3.eth.personal.unlockAccount(gethAddress, '', 100000)
+  await sleep(2000)
+  await kit.web3.eth.personal.unlockAccount(address, '', 100000)
 })
 
 after(async () => {
-  await kit.web3.eth.personal.lockAccount(gethAddress)
+  await kit.web3.eth.personal.lockAccount(address)
 })
